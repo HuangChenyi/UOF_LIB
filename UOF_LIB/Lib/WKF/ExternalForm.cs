99d0b46b08d0a4adcf598c6028c18ada3dbbad22
woodman <woodman@KH-WOODMAN-2017>
2018-05-02 11:27:31 +0800

    加入WS起單API

---
+++ UOF_LIB/Lib/WKF/ExternalForm.cs
@@ -0,0 +1,21 @@
+﻿using Ede.Uof.Utility.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.WKF
+{
+    public class ExternalForm
+    {
+        public string SendForm(string formInfo)
+        {
+            Setting setting = new Setting();
+            Form.TransferFormWS ws = new Form.TransferFormWS();
+            ws.Url = setting["SiteUrl"] + "/WKF/WebService/TransferFormWS.asmx";
+
+            return ws.SendForm(formInfo);
+        }
+    }
+}
