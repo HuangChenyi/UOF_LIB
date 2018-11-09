6bf9778c774ce7833885b1b5620e5802fb278e2b
woodman <woodman@KH-WOODMAN-2017>
2018-11-09 17:33:44 +0800

    加入測試程式

---
+++ UOF_LIB/Lib/WKF/TaskPro.cs
@@ -0,0 +1,29 @@
+﻿using Ede.Uof.WKF.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Lib.WKF
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TaskPro
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private Task m_Task = null;
+        public Task Task { get { return m_Task; } }
+
+
+        public TaskPro(string taskId)
+        {
+            m_Task = new Task(taskId);
+            
+        }
+
+    }
+}
